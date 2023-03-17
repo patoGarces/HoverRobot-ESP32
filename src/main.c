48562a5b93ab66e363444e7db544d1ee644201ac
PatricioGarces <patricio.garces@outlook.com>
2023-03-17 00:15:15 -0300

    Integracion componente CAN_COMMS como submodulo

--- src/main.c
+++ src/main.c
@@ -6,7 +6,12 @@
 /* Incluyo componentes */
 #include "../components/MPU6050/include/MPU6050.h"
 #include "../components/TFMINI/include/tfMini.h"
+#include "../components/CAN_COMM/include/CAN_COMMS.h"
 
+
+#define GPIO_CAN_TX     14
+#define GPIO_CAN_RX     12
+#define UART_PORT_CAN   UART_NUM_1
 
 void app_main() {
     float angleX = 0.00;
@@ -14,6 +19,7 @@ void app_main() {
 
     mpu_init();
     tfMiniInit();
+    canInit(GPIO_CAN_TX,GPIO_CAN_RX,UART_PORT_CAN);
 
     while(1){
 
