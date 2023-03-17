d56403c3b042b3e65c15ac22fbf1e460f5a8078c
PatricioGarces <patricio.garces@outlook.com>
2023-03-16 23:49:01 -0300

    Integracion componente TFMINI como submodulo

--- src/main.c
+++ src/main.c
@@ -5,16 +5,22 @@
 
 /* Incluyo componentes */
 #include "../components/MPU6050/include/MPU6050.h"
+#include "../components/TFMINI/include/tfMini.h"
 
 
 void app_main() {
     float angleX = 0.00;
+    uint16_t distance = 0;
 
     mpu_init();
+    tfMiniInit();
 
     while(1){
+
         angleX = getAngle(AXIS_ANGLE_X);
-        printf("angleX: %f",angleX);
+        distance = tfMiniGetDist();
+
+        printf("angleX: %f  , distance: %d",angleX,distance);
         vTaskDelay(pdMS_TO_TICKS(100));
     
     }
