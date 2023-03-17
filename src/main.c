549d8ea5393004ed79cce637b62a35ae370e71aa
PatricioGarces <patricio.garces@outlook.com>
2023-03-16 23:36:15 -0300

    Integracion componente MPU640 como submodulo

--- src/main.c
+++ src/main.c
@@ -1,1 +1,21 @@
-void app_main() {}
+#include "driver/gpio.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "stdio.h"
+
+/* Incluyo componentes */
+#include "../components/MPU6050/include/MPU6050.h"
+
+
+void app_main() {
+    float angleX = 0.00;
+
+    mpu_init();
+
+    while(1){
+        angleX = getAngle(AXIS_ANGLE_X);
+        printf("angleX: %f",angleX);
+        vTaskDelay(pdMS_TO_TICKS(100));
+    
+    }
+}
