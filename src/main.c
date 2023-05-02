6498df52ff158221eb602f74a6b87d9aa63d9241
PatricioGarces <patricio.garces@outlook.com>
2023-05-02 11:11:50 -0300

    Lectura de angulos OK

--- src/main.c
+++ src/main.c
@@ -17,27 +17,34 @@
 #define GPIO_CAN_RX     12
 #define UART_PORT_CAN   UART_NUM_1
 
-void app_main() {
-    float angleX = 0.00;
-    // uint16_t distance = 0;
+extern QueueSetHandle_t newAnglesQueue;
 
+void app_main() {
+    // uint16_t distance = 0;
     uint8_t cont1=0,cont2=0,cont3=0,cont4=0;
 
     gpio_set_direction(PIN_LED , GPIO_MODE_OUTPUT);
     gpio_set_level(PIN_LED, 0);
 
+    bt_init();
     mpu_init();
     // tfMiniInit();
     // canInit(GPIO_CAN_TX,GPIO_CAN_RX,UART_PORT_CAN);
-    bt_init();
+    
 
     while(1){
 
-        angleX = getAngle(AXIS_ANGLE_X);
-        // distance = tfMiniGetDist();
+       printf("angleX: %f , angleY: %f\n",getAngle(AXIS_ANGLE_X),getAngle(AXIS_ANGLE_Y));
 
+        // distance = tfMiniGetDist();
         // printf("angleX: %f  , distance: %d\n",angleX,distance);
-         printf("angleX: %f \n",angleX);
+         
+
+        // float vNewAngles[3];
+        // if(xQueueReceive(newAnglesQueue,&vNewAngles,0)){
+        //     printf("angleX: %f \n",vNewAngles[0]);
+        // }
+        
         if(btIsConnected()){
             // btSendAngle(12.25,45.50,457.780);
             // btSendData(0,0,0);
@@ -80,7 +87,7 @@ void app_main() {
         // gpio_set_level(PIN_LED,1);
         vTaskDelay(pdMS_TO_TICKS(100));
         // gpio_set_level(PIN_LED,0);
-        vTaskDelay(pdMS_TO_TICKS(100));
+        // vTaskDelay(pdMS_TO_TICKS(100));
     
     }
 }
